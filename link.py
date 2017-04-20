b26bd54b7f647c312cf62f2b3240a67ecd880587
Sam Crang <sam.crang@gmail.com>
2017-04-20 14:42:11 +0100

    Add more relationships

--- link.py
+++ link.py
@@ -7,6 +7,30 @@ config = [{
     "to": "organisation",
     "curie": "local-authority-eng",
     "name": "UNDER_AUTHORITY_OF"
+}, {
+    "from": "food-authority",
+    "to": "organisation",
+    "curie": "local-authority-nir",
+    "name": "UNDER_AUTHORITY_OF"
+}, {
+    "from": "food-authority",
+    "to": "organisation",
+    "curie": "local-authority-sct",
+    "name": "UNDER_AUTHORITY_OF"
+}, {
+    "from": "food-authority",
+    "to": "organisation",
+    "curie": "local-authority-wls",
+    "name": "UNDER_AUTHORITY_OF"
+}, {
+    "from": "food-premises-rating",
+    "to": "food-premises",
+    "name": "RATED",
+    "reversed": True
+}, {
+    "from": "food-premises",
+    "to": "premises",
+    "name": "LOCATED_AT"
 }]
 
 for r in config:
@@ -16,7 +40,14 @@ MATCH (b:`{}`)
 WHERE b.`{}` = '{}:' + a.`{}`
 CREATE (b)-[:{}]->(a);""".format(r['curie'],r['from'],r['to'],r['curie'],r['curie'],r['name']))
     else:
-        print("""MATCH (a:`{}`)
+        if 'reversed' in r and r['reversed']:
+            print("""MATCH (a:`{}`)
+MATCH (b:`{}`)
+WHERE a.`{}` = b.`{}`
+AND a <> b
+CREATE (a)-[:{}]->(b);""".format(r['to'],r['from'],r['to'],r['to'],r['name']))
+        else:
+            print("""MATCH (a:`{}`)
 MATCH (b:`{}`)
 WHERE a.`{}` = b.`{}`
 AND a <> b
